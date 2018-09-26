a4049d7454f12f48b10b3adf37844d0831cc2533
Sahan Rodrigo <sahan.rodrigo@eis.kent.gov.uk>
2018-09-26 08:09:23 +0100

    XUnit project added with UserService tests.

---
+++ tests/SR.AutoMapper.Sample.UnitTests/UserServiceTests.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using SR.AutoMapper.Sample.Domain.Repositories;
+using SR.AutoMapper.Sample.Services;
+using SR.AutoMapper.Sample.Services.Configurations.AutoMapper;
+using Xunit;
+
+namespace SR.AutoMapper.Sample.UnitTests
+{
+    public class UserServiceTests
+    {
+        [Fact]
+        public void ShouldGetListOfUsers()
+        {
+            var userAutoMapperProfile = new UserDomainDtoProfile();
+            var autoMapperConfiguration = new MapperConfiguration(cfg => cfg.AddProfile(userAutoMapperProfile));
+            var mapper = new Mapper(autoMapperConfiguration);
+
+            var userRepository = new UserRepository();
+            var sut = new UserService(mapper, userRepository);
+
+            var users = sut.GetUsers();
+
+            Assert.Equal(3, users.Count);
+        }
+    }
+}
