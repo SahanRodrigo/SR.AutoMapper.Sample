4528892687011b47313e1fa27a9529fcc13d6845
Rodrigo <s.rodrigo@vmd.gov.uk>
2019-07-10 09:54:10 +0100

    Updated domain project.

--- src/SR.AutoMapper.Sample.Domain/Extensions/DomainServiceCollectionExtension.cs
+++ src/SR.AutoMapper.Sample.Domain/Extensions/DomainServiceCollectionExtension.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using SR.AutoMapper.Sample.Domain.Repositories;
 
